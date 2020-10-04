b1d2413ab6b487fb2a6ad4e96383652ba10519d1
Vitor Alves Pereira <pereira.alves.vitor@outlook.com>
2020-10-04 09:19:46 -0300

    :octocat:

--- web/js/script.js
+++ web/js/script.js
@@ -1,7 +1,7 @@
 if (window.SimpleSlide) {
   new SimpleSlide({
-    slide: "quote",
-    time: 5000,
+    slide: "quote", // nome do atributo data-slide="principal"
+    time: 5000, // tempo de transição dos slides
   });
 
   new SimpleSlide({
